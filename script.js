3b75333af30bae7e3a7c80882d535f9ee8a902c0
quocbinh-npm9081 <quocbinhgithub@gmail.com>
2021-11-16 17:14:25 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// const navi = document.querySelector("nav");
+// const a = navi.querySelectorAll("a");
+
+// a.forEach(e => {
+//     e.addEventListener('click', function() {
+//         for (let i = 0; i < a.length; i++) {
+//             a[i].classList.remove("active");
+//         }
+//         this.classList.add("active");
+//     });
+// })
